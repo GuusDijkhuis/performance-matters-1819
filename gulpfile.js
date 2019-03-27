a848990cea5520be9672804b0836e91e79cde2dd
Guus <guus.dijkhuis@gmail.com>
2019-03-27 10:39:19 +0100

    add compression

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,38 @@ const gulp = require('gulp'); 
 const uglify = require('gulp-uglify'); 
 const cleanCSS = require('gulp-clean-css'); 
 const rename = require('gulp-rename'); 
+const rev = require('gulp-rev'); 
+const revReplace = require('gulp-rev-replace');
 
-const baseDir = 'assets/'; 
 
-
-gulp.task('jquery-min', function() {
-  return gulp.src(baseDir + '/js/jquery.js') 
+gulp.task('jquery-min', async () => {
+  await gulp.src('assets/js/jquery.js') 
     .pipe(uglify()) 
     .pipe(rename(function (path) {
       path.basename += "-min";
       path.extname = ".js";
     }))
-    .pipe(gulp.dest('public/js/'));
+    .pipe(gulp.dest('public/'));
 });
 
-gulp.task('css-min', () => {
-  return gulp.src(baseDir + '/css/main.css')
+gulp.task('css-min', async () => {
+  await gulp.src('assets/css/main.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename(function (path) {
       path.basename += "-min";
       path.extname = ".css";
     }))
-    .pipe(gulp.dest('public/css/'));
+    .pipe(gulp.dest('public/'));
 });
+
+gulp.task('revision', async () => {
+
+
+  await gulp.src([ 
+    'public/**/*.{css,js}' 
+  ]) 
+  .pipe(rev()) 
+  .pipe(gulp.dest('public/')) 
+  .pipe(rev.manifest('rev-manifest.json')) 
+  .pipe(gulp.dest('public/'));
+})
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const fs = require('fs');
 const path = require('path')
 const express = require('express');
-const compression = require('compression')
 const hbs = require('express-handlebars')
+const shrinkRay = require('shrink-ray-current')
 
 const routes = require('./assets/js/routeHandler.js')
 
 const app = express()
-const port = 8080
+const port = 3000
 
-app.get('/', routes.homepage)
-app.get('/details/:id', routes.detailpage)
 
 // Server side
 app.engine('hbs', hbs({ extname: 'hbs' }));
@@ -18,11 +16,46 @@ app.set('views', path.join(__dirname, '/assets/view/'))
 app.set('view engine', 'hbs')
 
 // Client side
-app.use(compression())
+
+app.use(function (req, res, next) {
+  res.locals = {
+    cssFilepath: revUrl("main-min.css"),
+    jsFilepath: revUrl("jquery-min.js")
+  };
+  next();
+});
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'max-age=' + 365 * 24 * 60 * 60);
   next();
 });
+app.use(shrinkRay({ 
+  filter: (req) => req.headers['accept'].includes('text/html') 
+}));
+
 app.use(express.static(__dirname + '/public'))
 
+app.get(['*.js', '*.css'], (req, res, next) => {
+  const encoding = req.headers['accept-encoding']
+  const extensionIndex = req.originalUrl.lastIndexOf('.')
+  const extension = req.originalUrl.slice(extensionIndex)
+
+  if (encoding && encoding.includes('br')) {
+    req.url = `${req.url}.br`
+    res.set('Content-Encoding', 'br')
+  } else if (encoding && encoding.includes('gzip')) {
+    req.url = `${req.url}.gz`
+    res.set('Content-Encoding', 'gzip')
+  }
+
+  res.set('Content-Type', extension === '.js' ? 'text/javascript' : 'text/css')
+  next()
+})
+app.get('/', routes.homepage)
+app.get('/details/:id', routes.detailpage)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+function revUrl(url) {
+  let fileName = JSON.parse(fs.readFileSync("public/rev-manifest.json", 'utf8'))
+  return fileName[url]
+}
