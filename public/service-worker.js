b80cb47d3f796c46d2165a65e4ae7d3338a4ff86
Guus <guus.dijkhuis@gmail.com>
2019-05-21 13:13:34 +0200

    service worker update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,16 @@
-
-// const cacheInstall = [
-// '/',
-// '/offline',
-// '/main-min.css',
-// '/jquery-min.js'
-// ]
-// const cacheFetch = [
-// '/details/*'
-// ]
-
+const cacheName = 'core-cache'
+const cacheInstall = [
+  '/offline',
+  '/main-min.css',
+  '/jquery-min.js'
+]
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches
     .open('core-cache')
     .then(cache => {
-      cache.addAll(['/offline'])
+      return cache.addAll(cacheInstall)
     })
     .then(() => {
       self.skipWaiting()
@@ -28,41 +23,33 @@ self.addEventListener('activate', (e) => {
 })
 
 self.addEventListener('fetch', (e) => {
-  console.log("Fetch event");
   e.respondWith(
-   caches.match(e.request)
-     .then(function(res) {
-       if (res) {
-         return res
-       }
-       return fetch(e.request)
-     })
+    fetch(e.request)
+    .then(res => {
+      caches
+        .open(cacheName)
+        .then(cache => {
+          cache.put(e.request.url, res.clone());
+        })
+      return res;
+    })
+    .catch(err => {
+      return caches.match(e.request.url)
+      .then(res => {
+        if (res) {
+          return res
+        } else {
+          return caches
+          .open(cacheName)
+          .then(cache => {
+            return cache.match('/offline')
+            .then(response => {
+              return response
+            })
+          })
+        }
+      })
+
+    })
   )
 })
-
-// self.addEventListener('fetch', (e) => {
-//   e.respondWith(fetch(e.request.url)
-//   .then(res => {
-//     return caches.open('html-cache')
-//     .then(cache => {
-//       return cache.put(e.request.url, res.clone())
-//       .then(() => {
-//         return res
-//       })
-//     })
-//   })
-//   .catch(err => {
-//     return caches.open('html-cache')
-//     .then(cache => {
-//       return caches.match(e.request.url).then(res => {
-//         console.log(res);
-//         res ? res : caches.open('core-cache')
-//         .then(cache => {
-//           return cache.match('/offline').then(res => {
-//             return res
-//           })
-//         })
-//       })
-//     })
-//   }))
-// })
